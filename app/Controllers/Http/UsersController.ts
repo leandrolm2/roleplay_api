2b1832476d029179f9ba31eacc2020c614d6de91
leandrolm2 <leandromelgarejo@hotmail.com>
2022-05-22 20:22:57 -0300

    add failed test to users

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -4,6 +4,8 @@ import User from 'App/Models/User'
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const userPayload = request.only(['email', 'username', 'password', 'avatar'])
+    const isEmail = await User.findBy('email', userPayload.email)
+    if (isEmail) return response.status(409).send('email already been used')
     const user = await User.create(userPayload)
     return response.created({ user })
   }
--- tests/functional/user.spec.ts
+++ tests/functional/user.spec.ts
@@ -1,8 +1,27 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { assert } from '@japa/preset-adonis'
 import { test } from '@japa/runner'
+import { UserFactory } from 'Database/factories'
 
-test('display welcome page', async ({ client }) => {
-  const response = await client.get('/')
+test.group('user', async (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
 
-  response.assertStatus(200)
-  response.assertBodyContains({ hello: 'world' })
+  test('it should create a user', async ({ client }) => {
+    const user = await UserFactory.make()
+    const response = await client.post('/user').json(user)
+    response.assertStatus(201)
+  })
+
+  test('it should failed to create user', async ({ client, assert }) => {
+    const users = await UserFactory.create()
+    const response = await client.post('/user').json(users)
+
+    // console.log(body)
+    // response.assertBodyContains('email')
+    // response.assertTextIncludes('BAD_REQUEST')
+    response.assertStatus(409)
+  })
 })
